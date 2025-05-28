0f9818bff9f4ef5fde4727ed9f71553be0c45393
Farmata Kane <kane.farmata@gmail.com>
2025-05-28 13:57:50 +0100

    Implement basic search form with API search

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import LandingPage from "./pages/LandingPage"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import './App.css'
+import SearchPage from "./pages/SearchPage";
 
 function App() {
  
@@ -11,7 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       {/* <Route path="/" element={<h1 className="text-orange-500 text-4xl font-bold">Hello Vite + React!</h1>} /> */}
-      
+      <Route path="/search" element={<SearchPage/>} />
     </Routes>
    </Router>
   )
