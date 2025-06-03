030293a6e6ca95b8bd44b3df976fb794579dec72
Maryamh12 <maryam_hazrati94@yahoo.com>
2025-06-03 21:44:20 +0100

    Setup functionality logins for  frontend

---
+++ Frontend/src/pages/SearchAIPage.tsx
@@ -0,0 +1,13 @@
+export default function SearchAIPage() {
+    return (
+        <div className="w-full bg-[#1a1a17] flex flex-col min-h-screen overflow-hidden relative">
+        <div className="flex flex-col mt-40 justify-center items-center">
+            <h1 className="text-2xl text-white mb-4">Search AI Page</h1>
+            <p className="text-white">This is a placeholder for the Search AI functionality.</p>
+        </div>
+        </div>
+    );
+    }
+
+
+    
